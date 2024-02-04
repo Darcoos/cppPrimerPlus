0084732ae53f739453cb4218290829f50cee5721
Darcoos <2670604162@qq.com>
2024-02-04 20:43:48 +0800

    test001

---
+++ Else/test001.cpp
@@ -0,0 +1,22 @@
+/*
+  @File    :   test001.cpp
+  @Folder  :   Else\test001.cpp
+  @Time    :   2024/02/04 20:43:33
+  @Author  :   »¨³ÇÎáÃû
+  @Contact :   2670604162@qq.com
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+
+    system("pause");
+    return 0;
+}
